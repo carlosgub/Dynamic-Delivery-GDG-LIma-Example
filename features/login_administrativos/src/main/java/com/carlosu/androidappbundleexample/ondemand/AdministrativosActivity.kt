1554e30b9b1628ff6d4be9ab4433df7d2be63695
CarlosU <carlosgabrielugazburga@gmail.com>
2018-10-24 08:27:37 -0500

    Update

---
+++ features/login_administrativos/src/main/java/com/carlosu/androidappbundleexample/ondemand/AdministrativosActivity.kt
@@ -0,0 +1,13 @@
+package com.carlosu.androidappbundleexample.ondemand
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.carlosu.androidappbundleexample.ondemand.login_administrativos.R
+
+class AdministrativosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_administrativos)
+    }
+}
